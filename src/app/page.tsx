ebb8fd77d57d15a368ec4c552e6b679624905396
Daniil <baldindaniilza@mail.ru>
2025-01-02 02:04:22 +0500

    Фикс для билда #8 Откат до более ранних версий

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-// import { Home } from "@pages/Home/Home.tsx";
-import { Home } from "@/_pages/Home/Home";
+import { Home } from "@pages/Home/Home";
+// import { Home } from "@/_pages/Home/Home";
 
 export default function HomePage() {
   return (
