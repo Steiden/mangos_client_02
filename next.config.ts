ec6c4980b5c5084013ed5aa85a9742e23e6912a4
Daniil <baldindaniilza@mail.ru>
2025-01-02 01:49:05 +0500

    Фикс для билда #5

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   "sassOptions": {
     "silenceDeprecations": ['legacy-js-api'],
-  }
+  },
+  "pageExtensions": ["tsx", "ts", "js", "jsx"],
+  "webpack": (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": __dirname,
+      "@app": __dirname + "/src/app",
+      "@pages": __dirname + "/src/_pages",
+      "@widgets": __dirname + "/src/widgets",
+      "@features": __dirname + "/src/features",
+      "@entities": __dirname + "/src/entities",
+      "@shared": __dirname + "/src/shared",
+      "@styles": __dirname + "/src/shared/styles",
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
