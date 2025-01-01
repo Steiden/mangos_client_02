96fc3dc47450b65d68c18d6bb99aa83fd6fb271b
Daniil <baldindaniilza@mail.ru>
2025-01-02 02:20:39 +0500

    Фикс для билда #9

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,21 @@
 import type { NextConfig } from "next";
-import { resolve } from "path";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  // "sassOptions": {
-  //   "silenceDeprecations": ['legacy-js-api'],
-  // },
-  // "pageExtensions": ["tsx", "ts", "js", "jsx"],
-  // "webpack": (config) => {
-  //   config.resolve.alias['@'] = resolve(__dirname, './src');
-  //   config.resolve.alias['@app'] = resolve(__dirname, './src/app');
-  //   config.resolve.alias['@pages'] = resolve(__dirname, './src/_pages');
-  //   config.resolve.alias['@widgets'] = resolve(__dirname, './src/widgets');
-  //   config.resolve.alias['@features'] = resolve(__dirname, './src/features');
-  //   config.resolve.alias['@entities'] = resolve(__dirname, './src/entities');
-  //   config.resolve.alias['@shared'] = resolve(__dirname, './src/shared');
-  //   config.resolve.alias['@styles'] = resolve(__dirname, './src/shared/styles');
-
-  //   return config;
-  // },
+	webpack: (config) => {
+		config.resolve.alias = {
+			...config.resolve.alias,
+			"@": path.resolve(__dirname, "src"),
+			"@app": path.resolve(__dirname, "src/app"),
+			"@pages": path.resolve(__dirname, "src/_pages"),
+			"@widgets": path.resolve(__dirname, "src/widgets"),
+			"@features": path.resolve(__dirname, "src/features"),
+			"@entities": path.resolve(__dirname, "src/entities"),
+			"@shared": path.resolve(__dirname, "src/shared"),
+			"@styles": path.resolve(__dirname, "src/shared/styles"),
+		};
+        return config;
+	},
 };
 
 export default nextConfig;
