fe66815decf660151279e718a820d0b18d0ed305
Daniil <baldindaniilza@mail.ru>
2025-01-02 01:58:03 +0500

    Фикс для билда #7

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,22 @@ import type { NextConfig } from "next";
 import { resolve } from "path";
 
 const nextConfig: NextConfig = {
-  "sassOptions": {
-    "silenceDeprecations": ['legacy-js-api'],
-  },
-  "pageExtensions": ["tsx", "ts", "js", "jsx"],
-  "webpack": (config) => {
-    config.resolve.alias['@'] = resolve(__dirname, './src');
-    config.resolve.alias['@app'] = resolve(__dirname, './src/app');
-    config.resolve.alias['@pages'] = resolve(__dirname, './src/_pages');
-    config.resolve.alias['@widgets'] = resolve(__dirname, './src/widgets');
-    config.resolve.alias['@features'] = resolve(__dirname, './src/features');
-    config.resolve.alias['@entities'] = resolve(__dirname, './src/entities');
-    config.resolve.alias['@shared'] = resolve(__dirname, './src/shared');
-    config.resolve.alias['@styles'] = resolve(__dirname, './src/shared/styles');
+  // "sassOptions": {
+  //   "silenceDeprecations": ['legacy-js-api'],
+  // },
+  // "pageExtensions": ["tsx", "ts", "js", "jsx"],
+  // "webpack": (config) => {
+  //   config.resolve.alias['@'] = resolve(__dirname, './src');
+  //   config.resolve.alias['@app'] = resolve(__dirname, './src/app');
+  //   config.resolve.alias['@pages'] = resolve(__dirname, './src/_pages');
+  //   config.resolve.alias['@widgets'] = resolve(__dirname, './src/widgets');
+  //   config.resolve.alias['@features'] = resolve(__dirname, './src/features');
+  //   config.resolve.alias['@entities'] = resolve(__dirname, './src/entities');
+  //   config.resolve.alias['@shared'] = resolve(__dirname, './src/shared');
+  //   config.resolve.alias['@styles'] = resolve(__dirname, './src/shared/styles');
 
-    return config;
-  },
+  //   return config;
+  // },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Home } from "@pages/Home/Home.tsx";
+// import { Home } from "@pages/Home/Home.tsx";
+import { Home } from "@/_pages/Home/Home";
 
 export default function HomePage() {
   return (
