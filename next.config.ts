9a8e0d8feeb9ca5740ab21f010336eee06f5f78b
Daniil <baldindaniilza@mail.ru>
2025-01-02 01:52:42 +0500

    Фикс для билда #6

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { resolve } from "path";
 
 const nextConfig: NextConfig = {
   "sassOptions": {
@@ -6,17 +7,14 @@ const nextConfig: NextConfig = {
   },
   "pageExtensions": ["tsx", "ts", "js", "jsx"],
   "webpack": (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": __dirname,
-      "@app": __dirname + "/src/app",
-      "@pages": __dirname + "/src/_pages",
-      "@widgets": __dirname + "/src/widgets",
-      "@features": __dirname + "/src/features",
-      "@entities": __dirname + "/src/entities",
-      "@shared": __dirname + "/src/shared",
-      "@styles": __dirname + "/src/shared/styles",
-    };
+    config.resolve.alias['@'] = resolve(__dirname, './src');
+    config.resolve.alias['@app'] = resolve(__dirname, './src/app');
+    config.resolve.alias['@pages'] = resolve(__dirname, './src/_pages');
+    config.resolve.alias['@widgets'] = resolve(__dirname, './src/widgets');
+    config.resolve.alias['@features'] = resolve(__dirname, './src/features');
+    config.resolve.alias['@entities'] = resolve(__dirname, './src/entities');
+    config.resolve.alias['@shared'] = resolve(__dirname, './src/shared');
+    config.resolve.alias['@styles'] = resolve(__dirname, './src/shared/styles');
 
     return config;
   },
