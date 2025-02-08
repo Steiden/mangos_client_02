f23360571e87f4facb3a1039e9eedff2ce023463
Daniil <baldindaniilza@mail.ru>
2025-02-09 01:28:51 +0500

    Дополнил headers в request

--- src/shared/api/config.ts
+++ src/shared/api/config.ts
@@ -4,6 +4,9 @@ export const getAxiosConfig = (token: string = ""): AxiosRequestConfig => {
 	return {
 		headers: {
 			Authorization: "Bearer " + token,
+			"Content-Type": "application/json",
+            Accept: "application/json",
+			Connection: "Keep-Alive",
 		},
 	};
 };
